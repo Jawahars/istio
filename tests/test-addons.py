1a02fcbf32efbca79cfa5b7e8c1f61a79921fb76
Jawahar <Jawahar@ibm.com>
2023-11-03 12:50:44 +0530

    Update test-addons.py

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -1,3 +1,11 @@
+import sh
+import yaml
+from utils import microk8s_enable, wait_for_pod_state, microk8s_disable
+
 class TestAddons(object):
     def test_istio(self):
-        pass
+        microk8s_enable("istio")
+        wait_for_pod_state("", "default", "running", label="app=python-demo-nginx")
+        status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
+        expected = {"istio": "enabled"}
+        microk8s_disable("istio")
